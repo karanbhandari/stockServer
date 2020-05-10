80602ea0597e734928cd18fb88ac0dfe9179d45b
karannbhandari <karan7752@gmail.com>
2020-05-09 21:32:36 -0400

    added Procfile

--- server.py
+++ server.py
@@ -10,3 +10,5 @@ def hello():
 if __name__=='__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+
+# TODO: Calculate the ticker's interest Coverage ratio (The interest coverage ratio is defined as the ratio of a company’s operating income (or EBIT – earnings before interest or taxes) to its interest expense. The ratio measures a company’s ability to meet the interest expense on its debt with its operating income. )
