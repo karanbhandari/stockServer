8e52ddca6c8599dc6870c8fbe0b7aca222e93ad7
karannbhandari <karan7752@gmail.com>
2020-05-18 00:06:00 -0400

    added new quaterly data with stock price and other relevant metrics

---
+++ model/Quaterly_Data.py
@@ -0,0 +1,3 @@
+class Quaterly_Data:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
--- server.py
+++ server.py
@@ -6,6 +6,10 @@ from aiohttp import web
 import requests
 import json
 import async_timeout
+from datetime import date, datetime
+from collections import namedtuple
+
+# from model import Quaterly_Data
 
 
 loop = asyncio.get_event_loop()
@@ -63,14 +67,56 @@ def company_rating(ticker):
     result = loop.run_until_complete(call_my_api(url))
     return json.dumps(json.loads(result))
 
+@app.route('/test')
+def test():
+    res = stock_price_per_quarter('AAPL', 2)
+    return json.dumps(res)
+
+################################################
+# Models
+# ideally should be added to the models class
+
+class Quaterly_Data:
+    def __init__(self, **entries):
+        self.date = entries['date']
+        self.stock_price = float(entries['Stock Price'])
+        self.number_of_shares = float(entries['Number of Shares'])
+        self.market_capitalization = str(entries['Market Capitalization'])
+        self.cash_equivalent = str(entries['- Cash & Cash Equivalents'])
+        self.total_debt = str(entries['+ Total Debt'])
+        self.enterprise_value = str(entries['Enterprise Value'])
+
+
 
 #################################################
+# helper functions
 
+# returns the stock price result at the end of the every quarter along with the number of shares, Market Capitalization, cash and equivalents, total debt and enterprise value
+
+def stock_price_per_quarter(ticker, total_past_years_req):
+    url = 'https://financialmodelingprep.com/api/v3/enterprise-value/' + ticker + '?period=quarter'
+    result = json.loads(loop.run_until_complete(call_my_api(url)))
+    current_year = date.today().year
+    start_year = current_year - total_past_years_req 
+    response = []
+    for item in result["enterpriseValues"]:
+        data = Quaterly_Data(**item)
+        dt = datetime.strptime(data.date, '%Y-%m-%d')
+        if dt.year >= start_year:
+            response.append(data.__dict__)
+    return {'symbol': result['symbol'], ' data': response}
+    # print(response)
+    # return result
+
+
+
+# calls the stocks api asyncronously
 async def call_my_api(url):
     async with aiohttp.ClientSession() as session, async_timeout.timeout(10):
         async with session.get(url) as resp:
             return await resp.text()
 
+#################################################
 if __name__=='__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
